cc671815cc41082045951b0299b9ca937ec930c1
zhou-xucheng <3437952534@qq.com>
2026-06-25 14:21:59 +0800

    配置 GitHub Pages 自动部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: '/teaching-etf-model-demo/',
   build: {
     sourcemap: 'hidden',
   },
